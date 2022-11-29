850e23a8d8d7614444ad3ef9e1b6b133431be62a
Rachel <raa563@nyu.edu>
2022-11-29 10:21:01 -0500

    Added more routing tests

--- web-app/tests/app_test.py
+++ web-app/tests/app_test.py
@@ -16,3 +16,16 @@ def test_homepage_route_content():
      assert 'Welcome to Handscribe!' in html
      assert 'Extract text from PDFs and images' in html
      
+def test_homepage_POST_request():
+    url='/'
+    client = app.test_client()
+    response = client.post(url)
+    assert response.status_code==200
+
+def test_invalid_route():
+    url='/home'
+    client = app.test_client()
+    response = client.get(url)
+    assert response.status_code==404
+
+# def test_result_rendering():
