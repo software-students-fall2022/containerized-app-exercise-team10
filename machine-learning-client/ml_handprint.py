3f431a06116814cf069ac8452dd26be09b65e344
sarah-altowaity1 <sa5793@nyu.edu>
2022-11-28 21:14:50 -0500

    add comments

--- machine-learning-client/ml_handprint.py
+++ machine-learning-client/ml_handprint.py
@@ -3,21 +3,47 @@ from PIL import Image
 import os 
 import glob
 
+
 def get_images():
-    # get all the images in the working directory
+    '''function that fetches all images in the current 
+    directory 
+    TODO: this function will need to be rewritten to fetch
+    images from the web application. for now, it is getting the images
+    used for testing from the current directory.
+    '''
     images = glob.glob("*.jpg") + glob.glob("*.png") + glob.glob("*.jpeg")
     return images
 
 def get_annotated_images():
+    '''
+    this function retrieves the annontated images returned by
+    handprint.
+    '''
     images = glob.glob("*-microsoft.png")
     return images
 
 def get_raw_text_data():
+    '''
+    function returns the text read by handprint
+    '''
     text_files =  glob.glob("*-microsoft.txt")
     return text_files 
 
+def print_extracted_text(texts):
+    '''
+    function prints the text read from handwritten content 
+    read by handprint 
+    '''
+    for text in texts:
+        with open(text) as file:
+            print(" ".join(file.readlines()))
+        file.close()
 
 def delete_process_files():
+    '''
+    function used to clean the directory from the data output 
+    of the tool 
+    '''
     curr_dir = os.getcwd()
     files = os.listdir(curr_dir)
     for item in files:
@@ -27,10 +53,12 @@ def delete_process_files():
 if __name__ == "__main__":    
     # TODO:  # get image uploaded on the web application  
     # for now, we're using a sample image from Google 
-    images = get_images()
+    images = get_images() # fetch images in the local directory 
+    # call the tool on the images in the directory 
     for image in images:
+        print(image)
         os.system("handprint -s microsoft -e " + image)
-
     annotated_images = get_annotated_images()
-    texts = get_raw_text_data()
+    # TODO: save annontated imahes somewhere
+    print_extracted_text(get_raw_text_data())
     delete_process_files()
