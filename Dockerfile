793f3fbfebf07567a920b9b14f63055f1f2cd417
sarah-altowaity1 <sa5793@nyu.edu>
2022-12-01 02:57:36 -0500

    redesigned client as flask, enable saving images into local directory from flask web app, send post request with saved image to ml client

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM python:3.10.8
 ADD . /app
 WORKDIR /app
+RUN pip install requests
 RUN pip install -r requirements.txt
--- web-app/app.py
+++ web-app/app.py
@@ -1,9 +1,16 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, redirect, flash
 from werkzeug.utils import secure_filename
 import pymongo
 from pymongo import MongoClient
+import os
+import glob
+import requests
 
 app = Flask(__name__)
+UPLOAD_FOLDER = os.getcwd()
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = os.urandom(24)
 
 def get_db():
     client = MongoClient(host='test_mongodb',
@@ -14,14 +21,42 @@ def get_db():
     db = client["animal_db"]
     return db
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/', methods=['GET', 'POST'])
 def homepage():
     if request.method == 'GET':
         return render_template('index.html')
     # files = request.files['file']
     # files.save(secure_filename(files.filename))
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            print("hi")
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash("image uploaded")
+            send_image("http://localhost:5002/process")
+            print("here")
+            return  render_template('result.html')# TODO: this is just temporary
     return render_template('result.html')
 
+# @app.route('/send_image')
+def send_image(url_link):
+    images = glob.glob("*.jpg") + glob.glob("*.png") + glob.glob("*.jpeg")
+    payload = {"image": images[0]}
+    response = requests.post(url_link, files=payload)
 
 # SAMPLE CODE
 @app.route('/animals')
@@ -39,4 +74,4 @@ def get_stored_animals():
             db.close()
 
 if __name__=='__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=3000)
