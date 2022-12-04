94836cd629684ad110d7d6e32b39e782290143dc
mishaseo <ms11568@nyu.edu>
2022-12-04 16:01:14 -0500

    Added a post test and test for annotated images

--- machine-learning-client/tests/ml_test.py
+++ machine-learning-client/tests/ml_test.py
@@ -1,44 +1,52 @@
 
 import pytest
-import os
+import io
 from app import app
 from app import get_images
 from app import get_raw_text_data
 from app import get_extracted_text
 from app import process_image
+from app import get_annotated_images
 
 
 class Tests:
 
 #Valid Function Tests
 #-------------------------------------------------------------------
-    #Valid get_images test: Two files stored in directory
+    #Valid get_images test: One file stored in directory
     def test_get_image(self):
         images=get_images()
-        assert len(images)==1
+        assert len(images)==2
         assert images[0]=="testwrite.jpg"
-        assert (".jpg" in images[0])==True
+        assert ".jpg" in images[0]
+        assert ".png" in images[1]
 
     #Valid get_raw_text_data test
     def test_get_raw_text_data(self):
         file=get_raw_text_data()
-        #Make sure it gets the file
+        #Make sure it gets the correct file
         assert "txt" in file
         assert file=='testTxtFile-microsoft.txt'
         assert isinstance(file,str)
     
+    #Valid get_annotated_iamges() test
+    def test_get_annotated_images(self):
+        image=get_annotated_images()
+        assert len(image)==1
+        assert "testImage2-microsoft.png" in image
+       
     #Valid get_extracted_text test
     def test_get_extracted_text(self):
         insert=get_raw_text_data()
         text=get_extracted_text(insert)
+        assert len(text)!=0
         assert text=="Hi there all"
         assert isinstance(text,str)
 
-
-#NEED TO GET TEXT FROM AN ACTUAL IMAGE
-    #Valid process image
+    #Valid process_image test
     def test_process_image(self):
         text=process_image()
+        assert len(text)!=0
         assert isinstance(text,str)
         
 #Invalid Extraction Tests
@@ -48,7 +56,7 @@ class Tests:
         images=get_images()
         assert (".txt" in images[0])==False
 
-   #Invalid get_raw_text_data test: Checks that only text file is gotten 
+   #Invalid get_raw_text_data test: Checks that only text file is extracted 
     def test_invalid_get_raw_text_data_invalid(self):
         file=get_raw_text_data()
         assert (".jpg" in file)==False
@@ -56,21 +64,36 @@ class Tests:
 
 #Handle Route Tests
 #------------------------------------------------------------------
-    #ROUTE: (valid) route handler for GET request to '/process'
-    def test_process_get(self):
+    #ROUTE: (valid) route handler for GET request to '/'
+    def test_get(self):
          url='/'
          client = app.test_client()
          response = client.get(url)
          assert response.status_code==200
 
-    #ROUTE: (invalid) route handler for GET request to '/'
+    #ROUTE: (invalid) route handler for GET request to '/process' which does not exist
     def test_invalid_route(self):
         url='/process'
         client = app.test_client()
         response = client.get(url)
         assert response.status_code==404
-        
-#----------------------------------------------------------------
-    
 
 
+    #ROUTE: (valid) route handler for GET request to '/'
+    #Test with file sent and no file sent
+    def test_post(self):
+        file = "testwrite.jpg"
+        data = {
+        'image': (open(file, 'rb'), file)
+    }
+        url='/'
+        client = app.test_client()
+        response = client.post(url,data=data)
+        assert response.status_code == 400
+        #No file sent
+        response = client.post(url,data=None)
+        assert response.status_code == 400
+
+   
+
+   
