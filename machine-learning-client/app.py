ad5dafd10e5673431a916a17413ac1feb07ab258
danilo-montes <dem9855@nyu.edu>
2022-12-04 11:55:33 -0500

    delete og file

--- machine-learning-client/app.py
+++ machine-learning-client/app.py
@@ -1,26 +1,25 @@
 from flask import Flask, request,render_template, redirect, flash
-# from PIL import Image
 from pymongo import MongoClient
-import pymongo
 import os, glob, requests, sys
-# import handprint
 from werkzeug.utils import secure_filename
+# from PIL import Image
+# import pymongo
+# import handprint
 
-# setup
+
+
+
+################## setup ##################
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb://db:27017/project4'
-app.secret_key = os.urandom(24)
-
-def get_db():
-    client = MongoClient(host='db',
-                        port=27017)
-    return client
-
-
-
 UPLOAD_FOLDER = os.getcwd()
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = os.urandom(24)
+# app.config['MONGO_URI'] = 'mongodb://db:27017/project4'
+
+def get_db():
+    client = MongoClient(host='db', port=27017)
+    return client
 
 def allowed_file(filename):
     '''
@@ -30,7 +29,7 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-
+################## Handprint functions ##################
 def get_images():
     '''function that fetches all images in the current 
     directory 
@@ -67,7 +66,7 @@ def get_extracted_text(text):
     file.close()
     return complete_text
 
-def delete_process_files():
+def delete_process_files(filename):
     '''
     function used to clean the directory from the data output 
     of the tool 
@@ -75,64 +74,87 @@ def delete_process_files():
     curr_dir = os.getcwd()
     files = os.listdir(curr_dir)
     for item in files:
-        if item.endswith("-microsoft.txt") or item.endswith("-microsoft.png") or item.endswith("-microsoft.json"):
+        if  item.endswith("-microsoft.txt") or \
+            item.endswith("-microsoft.png") or \
+            item.endswith("-microsoft.json") or \
+            item.endswith(filename):
             os.remove(os.path.join(curr_dir, item))
-
-
-
-
 
 def process_image():
     images = get_images()
 
     for image in images:
         #     print(image)
+
         print("handprint -s microsoft -e " + image, file=sys.stderr)
+
+        # run handprint
         os.system("handprint -s microsoft -e " + image)
+
         # annotated_images = get_annotated_images()
         # # TODO: save annontated imahes somewhere
+
         text = get_extracted_text(get_raw_text_data())
+
         # delete_process_files()
         return text
     
-# routes
+
+
+
+################## routes ##################
 @app.route('/', methods=['GET','POST'])
 def process():
     if request.method == "GET":
+        return render_template('index.html')
+
+
     #     print(request, file=os.stderr)
     #     # TODO: get image from request and save it
     #     return "received"
-        return render_template('index.html')
     
+
+
+
     if request.method == "POST":
          # check that file exists
         if 'file' not in request.files:
-            redirect('/')    
+            redirect('/')
+        
         file = request.files['file']
         # if user does not select file, browser also
         # submit an empty part without filename
         if file.filename == '':
-            print("hi")
             return redirect('/')
+
         if file and allowed_file(file.filename):
+            # save file
             filename = secure_filename(file.filename)
             # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            file.save(secure_filename(file.filename))
-            text = process_image() # get text 
+            file.save(filename)
+
+            # get text 
+            text = process_image() 
             
+            # db connection
             client = get_db()
             db = client['project4']
 
+            # insert text into db
             id = db.images.insert_one({
                 'img_text' : text
             }).inserted_id
+
             print(db.images.find_one({'_id' : id} ), file=sys.stderr)
             print("here", file=sys.stderr)
             print(f'id:{id}', file=sys.stderr)
             print(db.list_collection_names(), file=sys.stderr)
-            delete_process_files() # delete uploaded images 
+
+            delete_process_files(filename) # delete uploaded images 
+
             client.close()
             
+            # go to web app for displaying results
             return redirect(f'http://localhost:3000/results?id={id}')
 
 
