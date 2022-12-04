4691007a82ea1bc6758d0b8e2f99a57a7d054880
shl622 <shl622@nyu.edu>
2022-12-04 15:54:49 -0500

    added css style reset

--- machine-learning-client/app.py
+++ machine-learning-client/app.py
@@ -142,4 +142,4 @@ def process():
 
 ################## run server ##################
 if __name__ == "__main__":    
-    app.run(host="0.0.0.0", port=3002)
+    app.run(host="0.0.0.0", port=3002, debug=True)
