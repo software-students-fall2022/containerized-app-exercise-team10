ab3415f1bf133f7f13a9e2ba6a72663c17cca581
mishaseo <ms11568@nyu.edu>
2022-12-04 14:58:07 -0500

    Added more tests and testing files

--- machine-learning-client/tests/ml_test.py
+++ machine-learning-client/tests/ml_test.py
@@ -1,5 +1,6 @@
 
 import pytest
+import os
 from app import app
 from app import get_images
 from app import get_raw_text_data
@@ -10,29 +11,41 @@ class Tests:
 
 #Valid Function Tests
 #-------------------------------------------------------------------
-    def test_get_images(self):
+    #Valid get_images test: Two files stored in directory
+    def test_get_image(self):
         images=get_images()
-        assert  len(images)==0
+        assert len(images)==1
 
-    #get_annotated_images
-    def test_get_extracted_text(self):
-        text=get_extracted_text(textTxt.txt)
-        assert isinstance(text,str)
-   
-
-
-#Invalid Function sTests
-#-------------------------------------------------------------------
-
+    #Valid get_raw_text_data test
+    def test_get_raw_text_data(self):
+        file=get_raw_text_data()
+        #Make sure it gets the file
+        assert file=='testTxtFile-microsoft.txt'
+        assert isinstance(file,str)
+    
+    #Valid get_extracted_text test
     def test_get_extracted_text(self):
         insert=get_raw_text_data()
         text=get_extracted_text(insert)
-        assert text==None
+        assert text=="Hi there all"
+        assert isinstance(text,str)
 
-   #Process image-no image error
+
+#NEED TO GET TEXT FROM AN ACTUAL IMAGE
+    #Valid process image
     def test_process_image(self):
-        text= process_image()
-        assert text==None
+        text=process_image()
+        assert isinstance(text,str)
+        
+#Invalid Function sTests
+#-------------------------------------------------------------------
+   
+   #Invalid get_raw_text_data test: Checks for incorrect file
+    def test_get_raw_text_data_invalid(self):
+        file=get_raw_text_data()
+        assert (file=='testTxtFile.txt')==False
+   #Process image-no image error
+   
 
 #Handle Route Tests
 #------------------------------------------------------------------
