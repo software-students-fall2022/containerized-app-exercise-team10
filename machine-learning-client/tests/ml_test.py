4721476aaad9fdcf9fe371f384db9e3d832b68c7
mishaseo <ms11568@nyu.edu>
2022-12-04 14:13:38 -0500

    Added ml-client tests (route and function)

--- machine-learning-client/tests/ml_test.py
+++ machine-learning-client/tests/ml_test.py
@@ -1,6 +1,56 @@
+
+import pytest
+from app import app
+from app import get_images
+from app import get_raw_text_data
+from app import get_extracted_text
+from app import process_image
+
 class Tests:
-    def test_sanity_check(self):
-        expected = True # the value we expect to be present
-        actual = True # the value we see in reality
-        assert actual == expected, "Expected True to be equal to True!"
+
+#Valid Function Tests
+#-------------------------------------------------------------------
+    def test_get_images(self):
+        images=get_images()
+        assert  len(images)==0
+
+    #get_annotated_images
+    def test_get_extracted_text(self):
+        text=get_extracted_text(textTxt.txt)
+        assert isinstance(text,str)
+   
+
+
+#Invalid Function sTests
+#-------------------------------------------------------------------
+
+    def test_get_extracted_text(self):
+        insert=get_raw_text_data()
+        text=get_extracted_text(insert)
+        assert text==None
+
+   #Process image-no image error
+    def test_process_image(self):
+        text= process_image()
+        assert text==None
+
+#Handle Route Tests
+#------------------------------------------------------------------
+    #ROUTE: (valid) route handler for GET request to '/process'
+    def test_process_get(self):
+         url='/'
+         client = app.test_client()
+         response = client.get(url)
+         assert response.status_code==200
+
+    #ROUTE: (invalid) route handler for GET request to '/'
+    def test_invalid_route(self):
+        url='/process'
+        client = app.test_client()
+        response = client.get(url)
+        assert response.status_code==404
+        
+#----------------------------------------------------------------
+    
+
 
